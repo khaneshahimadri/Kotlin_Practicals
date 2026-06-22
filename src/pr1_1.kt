3cdf78008c62f2ed8c12fc26deb09a040eed7e6d
khaneshahimadri <himadrikhanesha019@gmail.com>
2026-06-22 14:29:44 +0530

    pr1_1

---
+++ src/pr1_1.kt
@@ -0,0 +1,23 @@
+fun main() {
+    var i=1
+    var j:Int=22
+    var k:Float=1.5f
+    var l:Char='T'
+    var m:String="HMS"
+    var n:Boolean=false
+    var a:Double=96.36
+    var b:Long=338927847
+    var c:Short=-2
+    var d:Byte=127
+    println("i="+i)
+    println("j="+j)
+    println("k="+k)
+    println("l="+l)
+    println("m="+m)
+    println("n="+n)
+    println("a="+a)
+    println("b="+b)
+    println("c="+c)
+    println("d="+d)
+    println("Hello, world!!!")
+}
