8fe16e2c98ff1ef1713b453baaf871d4025cbad3
khaneshahimadri <himadrikhanesha019@gmail.com>
2026-06-22 20:42:01 +0530

    pr1_2

--- src/pr1_2.kt
+++ src/pr1_2.kt
@@ -1,5 +1,22 @@
 fun main() {
-    val integerNumber: Int = 46
-    val doubleNumber: Double = integerNumber.toDouble()
-    println(doubleNumber)
+
+    val intValue = 10
+    val doubleFromInt = intValue.toDouble()
+
+
+    val strValue = "10"
+    val intValue1 = strValue.toInt()
+    val intValue2 = Integer.parseInt(strValue)
+
+
+    val strDouble = "11.12"
+    val doubleFromString = strDouble.toDouble()
+
+
+    println("Integer Value:$intValue")
+    println("Double Value (From Integer):$doubleFromInt")
+    println("String Value:$strValue")
+    println("Integer Value1 (From String):$intValue1")
+    println("Integer Value2 (From String):$intValue2")
+    println("Double Value (From String):$doubleFromString")
 }
